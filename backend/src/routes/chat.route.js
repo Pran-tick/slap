54df57fef1108386a84def4a87f42a3b6cc515fc
Prantik Verma <prantikkvermaa4@gmail.com>
2025-09-04 03:08:20 +0530

    fix the 404 error

--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import { getStreamToken } from '../controllers/chat.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+import express from "express";
+import { getStreamToken } from "../controllers/chat.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// Define your chat routes here
-router.get("/token", protectRoute, getStreamToken)
+router.get("/token", protectRoute, getStreamToken);
 
 export default router;
